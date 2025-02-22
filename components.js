28b9514d58828e4e687ef6c0355ee95f8c7c0358
adolfosucumula <rocketmc2009@gmail.com>
2025-02-22 15:15:07 +0000

    some changes

--- components.js
+++ components.js
@@ -1,72 +1,10 @@
-class AuthJWTNoCookies{
-    constructor(){
+
+class AuthUser {
+    constructor() {
         //this.baseURL = window.location.origin;
         this.baseURL = window.location.origin + '/laurindo-c-benjamim-portfolio';
         this.serverDomain = 'https://www.d-tuning.com';
         //this.serverDomain = 'http://localhost:5000';
-    }
-
-    async getOptionJWT(method, body){
-        // getting the jwt token 
-        const token = localStorage.getItem('jwtToken')? localStorage.getItem('jwtToken') : null;
-        
-        // Create headers
-        const headers = token? {
-            'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`,
-        } : {'Content-Type': 'application/json'};
-
-        return method==='POST'? {
-            methods: method,
-            body: body,
-            credentials: 'same-origin',
-            headers: headers,
-        } : {
-            methods: method, credentials: 'same-origin', headers: headers,
-        };
-
-    };
-
-    async login(endpoint, formData){ 
-        const route = `${this.serverDomain}/${endpoint}`;
-        const options = this.getOptionJWT('POST', formData)
-        console.log("Options...")
-        console.log(options)
-        const response = await fetch(route, 
-        options)
-        .then(response => response.json());
-        console.log(response.json())
-        return response.json();
-    }
-
-    async logout(){
-        localStorage.removeItem('jwtToken')
-        window.location.href = this.baseURL + '/login_without_cookies.html'
-    }
-
-    async callProtectedRoute(endpoint, formData){
-        if(formData){
-            const response = await fetch(this.serverDomain + '/'+endpoint, 
-        this.getOptionJWT('GET', formData))
-        .then(response => response.json())
-        return ;
-        }
-
-        const response = await fetch(this.serverDomain + '/'+endpoint, 
-        this.getOptionJWT('GET', formData))
-        .then(response => response.json())
-        return response.json();
-    }
-};
-
-//alert("Ola")
-
-
-class AuthUser {
-    constructor() {
-        this.baseURL = window.location.origin;
-        //this.baseURL = window.location.origin + '/laurindo-c-benjamim-portfolio';
-        //this.serverDomain = 'https://www.d-tuning.com';
-        this.serverDomain = 'http://localhost:5000';
     }
     async login(options) {
         const response = await fetch(`${this.serverDomain}/login-w-cookies`, options);
